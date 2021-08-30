d2258ab3efc8d07958461136b88ae9f0432d429a
NamTran <nama3k67lnq@gmail.com>
2021-08-30 13:18:28 +0700

    upload exe chapter 15

---
+++ CH_15/Ex 15-3/library_slide_show_enhancements.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*** augment the slideshow object using the module pattern:                       ***
+ ***  - add an immediately invoked function expression (IIFE)                     ***
+ ***  - pass a parameter to the IIFE that will hold the module to be augmented    ***
+ ***  - import the module to be augmented by passing it as an argument when you   ***
+ ***      invoke the IIFE (it will populate the parameter in the last step)       ***
+ ***  - add the changeCaption method and return the parameter holding the module  ***/
+
+(function (mod) {
+  mod.changeCaption = function (i, newCaption) {
+    var currentImage = mod.getImages[i];
+    currentImage.title = newCaption;
+    return mod;
+  };
+})(myapp.slideshow);
