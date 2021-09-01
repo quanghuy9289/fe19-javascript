9f8b151be3a779c46e7e3a446b8ca5a639a819d4
Ngọc Dũng <ngocdungsss81@gmail.com>
2021-09-01 16:48:59 +0700

    Chuong:_15

---
+++ ch15/task_manager/library_storage.js
@@ -0,0 +1,21 @@
+"use strict";
+var mystorage = {
+    get: function() {
+        var storageItem = localStorage.getItem(this.key) || "";
+        return storageItem === "" ? [] : storageItem.split("|");
+    },
+    set: function(arr) {
+        if (Array.isArray(arr)) {
+            localStorage.setItem(this.key, arr.join("|"));
+        }
+    },
+    clear: function() {
+        localStorage.removeItem(this.key);
+    },
+
+};
+var getTaskStorage = function(key) {
+    var storage = Object.create(mystorage);
+    storage.key = key;
+    return storage;
+};
