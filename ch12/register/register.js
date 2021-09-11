3401e136e9c6bd98a60c93975c6deeff0feae793
your name <hieutrungphamtran@gmail.com>
2021-09-11 15:49:53 +0700

    add

---
+++ ch12/register/register.js
@@ -0,0 +1,25 @@
+"use strict";
+var registerForm;
+
+"use strict";
+var registerForm;
+
+onload = function () {
+  registerForm = new RegisterForm();
+  registerForm.resetForm();
+
+  $("register").onclick = function () {
+    if (registerForm.validateForm()) {
+      navigate.showResults();
+    }
+  };
+
+  $("reset").onclick = function () {
+    registerForm.resetForm();
+  };
+
+  $("back").onclick = function () {
+    navigate.showForm();
+    registerForm.resetForm();
+  };
+};
