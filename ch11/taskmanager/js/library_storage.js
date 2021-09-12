5368de6d95054570097ff11351d9bb6304c140fc
levantinh94 <levantinh.art94@gmail.com>
2021-09-12 18:52:16 +0700

    ch11

---
+++ ch11/taskmanager/js/library_storage.js
@@ -0,0 +1,21 @@
+var getStorage = function(key){
+    //get string from storage or an empty storage if nothing in storage
+    var storage = localStorage.getItem(key) || "";
+    if(storage === ""){
+        return [];
+    } else {
+        return storage.split("|");
+    }
+}
+
+var setStorage = function(key, arr){
+    if(Array.isArray(arr)){
+        var storageString = arr.join("|");
+        localStorage.setItem(key, storageString);
+
+    }
+}
+
+var clearStorage = function(key){
+    localStorage.setItem(key, "");
+}
